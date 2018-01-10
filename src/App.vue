7b245f8d4ff6bca11519c0ed59310b7de8bda0a9
yaoleia <18504228500@163.com>
2018-01-10 21:02:58 +0800

    s

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    LALALLAaaaa
+   <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Home from './containors/Home'
+export default {
+  components:{Home}
+}
+</script>
+
+<style scoped>
+</style>
